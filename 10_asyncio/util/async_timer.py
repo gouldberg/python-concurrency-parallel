6198399a5ea09fc4a9c71f59645dd908f0bdb922
gouldberg <gouldberg777@gmail.com>
2024-05-04 13:11:20 +0900

    20240504-pm2

---
+++ 10_asyncio/util/async_timer.py
@@ -0,0 +1,25 @@
+import functools
+import time
+from typing import Callable, Any
+
+
+# ----------------------------------------------------------------------------
+# decorator for timing coroutines
+# ----------------------------------------------------------------------------
+
+def async_timed():
+    def wrapper(func: Callable) -> Callable:
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs) -> Any:
+            print(f'Starting {func} with args {args} {kwargs}')
+            start = time.time()
+            try:
+                return await func(*args, **kwargs)
+            finally:
+                end = time.time()
+                total = end - start
+                print(f'finished {func} in {total: 4f} second(s)')
+        return wrapped
+    return wrapper
+
+
